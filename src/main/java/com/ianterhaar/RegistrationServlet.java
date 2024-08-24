e70bd55459e213c2cdb8dcfa5501690f3c106399
Ian Ter Haar <ian.terhaar1004@gmail.com>
2024-08-24 14:01:44 +0200

    add skeleton for registration servlet

---
+++ src/main/java/com/ianterhaar/RegistrationServlet.java
@@ -0,0 +1,22 @@
+package com.ianterhaar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Store the user details. For now, just redirect to login.
+        // In a real-world scenario, save to a database.
+        response.sendRedirect("login.html");
+    }
+}
